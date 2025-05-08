252c9c7d8c4342d4866cec7c88b6264a2d1f86ba
Yuhki <yuhkixx@protonmail.com>
2025-05-08 13:40:56 +0200

    new icon

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("zani.ico");
+        res.set_icon("cartethyia.ico");
         res.compile().unwrap();
     }
 }
